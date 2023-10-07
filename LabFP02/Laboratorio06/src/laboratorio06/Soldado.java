ccbf8e1631f0b7613344e2bc563d3e84c06cd443
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com>
2023-10-06 21:37:04 -0500

    Add files via upload

---
+++ LabFP02/Laboratorio06/src/laboratorio06/Soldado.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio06;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Soldado {
+    private String nombre;
+    private int fila;
+    private int columna;
+    private int nivelVida;
+    // Metodos mutadores
+    public void setNombre( String n){
+        nombre = n;
+    }
+    public void setFila(int f){
+        fila = f;
+    }
+    public void setColumna(int c){
+        columna = c;
+    }
+    
+    public void setNivelVida(int p){
+        nivelVida = p;
+    }
+    // Metodos accesores
+    public String getNombre(){
+        return nombre;
+    }
+    public int getFila(){
+        return fila;
+    }
+    public int getColumna(){
+        return columna;
+    }
+    
+    public int getNivelVida(){
+        return nivelVida;
+    }
+}
